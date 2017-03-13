4c96a06bacbf43c277ba040f58349f826fa0ed9a
HongJinhwan <hjh8402@gmail.com>
2017-03-13 17:26:24 +0900

    Add files via upload

---
+++ 지도 어플리케이션/main.js
@@ -0,0 +1,400 @@
+function myFunctions() {
+    document.getElementById("myDropdowns").classList.toggle("shows");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtns')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-contents");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shows')) {
+            openDropdown.classList.remove('shows');
+        }
+    }
+  }
+}
+
+function myFunctions2() {
+    document.getElementById("myDropdowns2").classList.toggle("shows2");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtns2')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-contents2");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shows2')) {
+            openDropdown.classList.remove('shows2');
+        }
+    }
+  }
+}
+
+function myFunctions3() {
+    document.getElementById("myDropdowns3").classList.toggle("shows3");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtns3')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-contents3");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shows3')) {
+            openDropdown.classList.remove('shows3');
+        }
+    }
+  }
+}
+
+function myFunctions4() {
+    document.getElementById("myDropdowns4").classList.toggle("shows4");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtns4')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-contents4");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shows4')) {
+            openDropdown.classList.remove('shows4');
+        }
+    }
+  }
+}
+
+function myFunctions5() {
+    document.getElementById("myDropdowns5").classList.toggle("shows5");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtns5')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-contents5");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shows5')) {
+            openDropdown.classList.remove('shows5');
+        }
+    }
+  }
+}
+
+function myFunctions6() {
+    document.getElementById("myDropdowns6").classList.toggle("shows6");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtns6')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-contents6");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shows6')) {
+            openDropdown.classList.remove('shows6');
+        }
+    }
+  }
+}
+
+function myFunctions7() {
+    document.getElementById("myDropdowns7").classList.toggle("shows7");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtns7')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-contents7");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shows7')) {
+            openDropdown.classList.remove('shows7');
+        }
+    }
+  }
+}
+function myFunctions8() {
+    document.getElementById("myDropdowns8").classList.toggle("shows8");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtns8')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-contents8");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shows8')) {
+            openDropdown.classList.remove('shows8');
+        }
+    }
+  }
+}
+
+function myFunctions9() {
+    document.getElementById("myDropdowns9").classList.toggle("shows9");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtns9')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-contents9");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('shows9')) {
+            openDropdown.classList.remove('shows9');
+        }
+    }
+  }
+}
+
+<!-- shows -->
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+    }
+  }
+}
+
+<!-- shows -->
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show2");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn2')) 
+    {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show2')) {
+            openDropdown.classList.remove('show2');
+        }
+    }
+  }
+}
+
+function myFunction3() {
+    document.getElementById("myDropdown3").classList.toggle("show3");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn3')) 
+    {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content3");
+    var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show3')) {
+            openDropdown.classList.remove('show3');
+        }
+    }
+  }
+}
+
+<!-- shows -->
+function myFunction4() {
+    document.getElementById("myDropdown4").classList.toggle("show4");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn4')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-content4");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show4')) {
+            openDropdown.classList.remove('show4');
+        }
+    }
+  }
+}
+
+<!-- shows -->
+function myFunction5() {
+    document.getElementById("myDropdown5").classList.toggle("show5");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn5')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-content5");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show5')) {
+            openDropdown.classList.remove('show5');
+        }
+    }
+  }
+}
+
+<!-- shows -->
+function myFunction6() {
+    document.getElementById("myDropdown6").classList.toggle("show6");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn6')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-content6");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show6')) {
+            openDropdown.classList.remove('show6');
+        }
+    }
+  }
+}
+
+<!-- shows -->
+function myFunction7() {
+    document.getElementById("myDropdown7").classList.toggle("show7");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn7')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-content7");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show7')) {
+            openDropdown.classList.remove('show7');
+        }
+    }
+  }
+}
+
+<!-- shows -->
+function myFunction8() {
+    document.getElementById("myDropdown8").classList.toggle("show8");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn8')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-content8");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show8')) {
+            openDropdown.classList.remove('show8');
+        }
+    }
+  }
+}
+
+<!-- shows -->
+function myFunction9() {
+    document.getElementById("myDropdown9").classList.toggle("show9");
+}
+
+<!-- click event -->
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn9')) 
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-content9");
+        var i;
+    for (i = 0; i < dropdowns.length; i++)
+    {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show9')) {
+            openDropdown.classList.remove('show9');
+        }
+    }
+  }
+}
+
+var start;
+var end;
+function startvalue(value){
+	start = value;
+	var ai = alert("출발지입력완료");
+}
+function endvalue(value){
+    end = value;
+	var ai = alert("도착지입력완료");
+}
+function sendFunc() {
+    location.href="Complete-insertdb.html?"+start+":"+end;
+}
